33f2575293c39d84f65c760800284e913ddcbf50
Darius Foo <darius.foo.tw@gmail.com>
2016-12-03 12:45:42 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+
+import git
+
+repo = git.Repo('/Users/darius/workspace/OfficeSimulatorThing')
+
+fifty_first_commits = list(repo.iter_commits('master', max_count=50))
+
+print fifty_first_commits
+
+print 'diffs'
+# TODO how to get blob content?
+print [[(diffobj.change_type, diffobj.a_blob) for diffobj in commit.diff(commit.parents[0])] for commit in fifty_first_commits if len(commit.parents) > 0]
+
+print 'parents'
+print [commit.parents for commit in fifty_first_commits]
+
+print 'blobs'
+# TODO how to get blob content?
+print [[blob for blob in list(commit.tree.traverse()) if blob.type == 'blob'] for commit in fifty_first_commits]
+
